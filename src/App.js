854a9dad4e1d6f3184d41f9784159f7b22314f2a
Andrea Paolini <andrea40paolini@gmail.com>
2022-08-13 23:32:11 +0200

    Ho messo bootstrap, il router e 1 pagina delle isole

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Link,
+  NavLink,
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Expenses from "./Expenses"
+import Vorso from "./Vorso"
+
+function DropDownItem(props) {
+  return <li><a className="dropdown-item" href={ props.href }>
+    { props.children }
+    </a></li>
+}
+function DropDownDivider() {
+  return <li><hr className="dropdown-divider" /></li>
+}
+
+function NavItem({href, children}) {  
+  return <li><NavLink className={({ isActive }) => (isActive ? "nav-link active" : "nav-link") } to={ href }>{ children }</NavLink></li>
+}
+
+function NavBar() {
+  return <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Kame</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <NavItem href="/expenses"> expenses </NavItem>
+        <li className="nav-item dropdown">
+          <a className={ "nav-link dropdown-toggle" + (window.location.pathname.startsWith("/Isole/") ? " active" : "")} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Isole
+          </a>
+          <ul className="dropdown-menu">
+            <DropDownItem href="/Isole/Vorso">Vorso</DropDownItem>
+            <DropDownItem href="/Isole/Torno">Torno</DropDownItem>
+            <DropDownItem href="/Isole/Sloto">Sloto</DropDownItem>
+            <DropDownDivider />
+            <DropDownItem href="/Isole/Corresant">Corresant</DropDownItem>
+            <DropDownItem href="/Isole/Nord">Nord</DropDownItem>
+            <DropDownItem href="/Isole/Bocca di Erebus">Bocca di Erebus</DropDownItem>
+            <DropDownItem href="/Isole/Lipari">Lipari</DropDownItem>
+            <DropDownItem href="/Isole/Zagros">Zagros</DropDownItem>
+            <DropDownDivider />
+            <DropDownItem href="/Isole/Chibetto">Chibetto</DropDownItem>
+          </ul>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+}
+
+function Home() {
+  return <p>Home!</p>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="Isole/Vorso" element={<Vorso />} />
+        </Routes>
     </div>
   );
 }
---
+++ src/Vorso.js
@@ -0,0 +1,37 @@
+export default function Vorso() {
+  return (
+      <div class="container-fluid my-4">
+      <div class="row">
+        <div class="col-2">
+          <ul class="list-group list-group-flush list-unstyled">
+            <li><a class="list-group-item rounded-top" href="#"><h5>Vorso</h5></a></li>
+            <li><a class="list-group-item" href="#">2</a></li>
+            <li><a class="list-group-item" href="#">3</a></li>
+            <li><a class="list-group-item" href="#">4</a></li>
+            <li><a class="list-group-item" href="#">5</a></li>
+            <li><a class="list-group-item" href="#">6</a></li>
+            <li><a class="list-group-item" href="#">7</a></li>
+            <li><a class="list-group-item" href="#">8</a></li>
+            <li><a class="list-group-item rounded-bottom" href="#">9</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <p> 
+            <h2>
+              Vorso
+            </h2>
+            Clima: Desertico.
+            <h4>
+              I demoni del deserto
+            </h4>
+            I Coboldi sono conosciuti anche come Demoni del deserto, in questa zona pricipalmente la sottorazza "Scinco". Questi si muovono sotto la sabbia e quando identificano una preda la attirano in una trappola di sabbia. Un Coboldo fa da esca mentre gli altri, nascosti sotto la sabbia, iniziano a far sprofondare la vittima che si avvicina.
+            <h4>
+              Carovane di Leonin
+            </h4>
+            I Leonin sono organizzati in carovane che viaggiano per tutta l'isola in cerca di risorse, tutte le carovane fanno però scalo alla valle del teschio, enorme oasi naturale, dove si fermano giorni a riposare, qui c'è una sorta di villaggio dei Leonin.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
